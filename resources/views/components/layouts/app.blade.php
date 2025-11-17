d1367dea8dbfeeb9e1a8bab4c2d19e9dea22ca7c
Nima8FT <nima_8ak@yahoo.com>
2025-11-17 12:25:00 +0330

    fix(vite): change if app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,17 +4,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 
     <title>Task Management System</title>
     @fluxAppearance
 </head>
 
 <body dir="rtl">
-<div class="container mx-auto p-4">
-    {{ $slot }}
-</div>
-@fluxScripts
+    <div class="container mx-auto p-4">
+        {{ $slot }}
+    </div>
+    @fluxScripts
 </body>
 
 </html>
