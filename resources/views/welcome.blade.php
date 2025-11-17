5ddf5286e35646511a096a7b8db5e2b0f734b9e7
Nima8FT <nima_8ak@yahoo.com>
2025-11-17 11:50:41 +0330

    fix(test): ignore vite in test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @if(!env('APP_ENV', 'testing'))
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if(env('APP_ENV', 'local'))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 
 <body>
-    <h1 class="text-red-900">Nima</h1>
+<h1 class="text-red-900">Nima</h1>
 </body>
 
 </html>
