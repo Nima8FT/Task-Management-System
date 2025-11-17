adf233f1177d549374b3e316d851e0c4a3c3ebe6
Nima8FT <nima_8ak@yahoo.com>
2025-11-17 12:32:35 +0330

    comment test

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
 
     <title>Task Management System</title>
     @fluxAppearance
