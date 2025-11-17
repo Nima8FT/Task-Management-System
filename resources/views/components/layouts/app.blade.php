e49379fe9380d8c79157c5828afe26fd3c8d9c29
Nima8FT <nima_8ak@yahoo.com>
2025-11-17 11:59:24 +0330

    fix(vite): ignore vite

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @if(env('APP_ENV', 'local'))
+    @if(!env('APP_ENV', 'testing'))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 
